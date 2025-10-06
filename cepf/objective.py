c5eae80810ef6a1ea5731ded8390dfa64431f910
antoine311200 <antoine311200@gmail.com>
2025-10-06 15:21:14 +0200

    Objective error function to minimize with Entropy and CrossEntropy objectives

---
+++ cepf/objective.py
@@ -0,0 +1,94 @@
+from abc import ABC, abstractmethod
+from functools import partial
+
+import numpy as np
+from scipy.optimize import minimize
+
+from .distribution import Distribution
+
+class Objective(ABC):
+
+    @abstractmethod
+    def evaluate(self, constraints, lambdas, *args, **kwargs) -> float:
+        pass
+
+    @abstractmethod
+    def gradient(self, constraints, lambdas, *args, **kwargs) -> np.ndarray:
+        pass
+
+    def minimize(self, constraints, lambdas, tol, max_iter):
+        """Minimize the objective function using L-BFGS-B"""
+
+        result = minimize(
+            fun=partial(self.evaluate, constraints),
+            jac=partial(self.gradient, constraints),
+            x0=lambdas,
+            method='L-BFGS-B',
+            options={'gtol': tol, 'maxiter': max_iter}
+        )
+        if not result.success:
+            raise RuntimeError("Optimization failed: " + result.message)
+
+        return {'optimal_lambdas': result.x, 'fun': result.fun, 'nfev': result.nfev}
+
+class CrossEntropyObjective(Objective):
+
+    def __init__(self, x: np.ndarray, dx: float) -> None:
+        self.x = x
+        self.dx = dx
+
+    def _pdf(self, lambdas, constraints, other_distribution: Distribution):
+        n_constraints = len(constraints)
+        exponent = np.zeros_like(self.x)
+        for i in range(n_constraints):
+            exponent += lambdas[i] * constraints[i]
+
+        # Avoid overflow
+        exponent = np.clip(exponent, -500, 500)
+        exp_exponent = np.exp(exponent) * other_distribution.pdf
+        mu = np.sum(exp_exponent) * self.dx
+
+        if mu <= 0:
+            raise ValueError("Normalization constant mu is non-positive.")
+
+        pdf = exp_exponent / mu
+        return pdf, mu
+
+    def evaluate(self, constraints, lambdas, prior_distribution: Distribution):
+        """Objective function F(λ) = log(μ) - Σ λ_i d_i"""
+        # Check that other_distribution has the same x and dx
+        if not np.array_equal(self.x, prior_distribution.x) or self.dx != prior_distribution.dx:
+            raise ValueError("Incompatible distributions")
+
+        _, mu = self._pdf(lambdas, constraints, prior_distribution)
+
+        targets = np.array([c.target for c in constraints])
+        value = np.log(mu) - np.sum(lambdas * targets)
+        return value
+
+    def gradient(self, constraints, lambdas, prior_distribution: Distribution):
+        """Gradient of the objective function ∇F(λ) = (E[c_i] - d_i)
+        The assumption is that the constraints are of the form E[c_i] = d_i"""
+
+        p, _ = self._pdf(lambdas, constraints, prior_distribution)
+        n_constraints = len(constraints)
+
+        gradient = np.zeros(n_constraints)
+        for i in range(n_constraints):
+            expected_value = np.sum(constraints[i] * p) * self.dx
+            gradient[i] = expected_value  - constraints[i].target
+
+        return gradient
+
+class EntropyObjective(CrossEntropyObjective):
+
+    def __init__(self, x: np.ndarray, dx: float) -> None:
+        super().__init__(x, dx)
+        self.no_prior = Distribution((x[0], x[-1]), len(x))
+        self.no_prior.pdf = np.ones_like(x)
+
+    def evaluate(self, constraints, lambdas, *args, **kwargs):
+        return super().evaluate(constraints, lambdas, self.no_prior)
+
+    def gradient(self, constraints, lambdas, *args, **kwargs):
+        return super().gradient(constraints, lambdas, self.no_prior)
